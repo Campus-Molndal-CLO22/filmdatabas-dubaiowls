768b7bde7361cb0e17a17a449f87ef1a1300b5ee
Alexander Öjersson <98aleoje@gafe.molndal.se>
2022-11-09 12:10:57 +0100

    gg

--- MovieDatabase_Template/Actor.cs
+++ MovieDatabase_Template/Actor.cs
@@ -3,18 +3,15 @@
     public class Actor
     {
         public int Id { get; set; }
-        public string Name;
+        public string Name { get; set; }
         public int Age { get; set; }
         public int BornYear { get; set; }
         public int MovieId { get; set; }
-
+        public string Movies { get; set; }
+       
         public Actor()
         {
-            Console.WriteLine("Enter the name of the actor: ");
-            Name = Console.ReadLine();
-            Console.WriteLine("Enter the age of the actor: ");
-            Age = int.Parse(Console.ReadLine());
-            Console.WriteLine();
+
         }
     }
 }
